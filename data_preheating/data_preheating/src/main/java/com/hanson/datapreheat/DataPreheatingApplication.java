a229fe1ce7c9a66bc42207f17736cc1f6164b4ef
hanson <hanson0622@126.com>
2024-07-11 01:35:04 +0800

    该项目旨在提供一个基于Spring Boot的简单示例，展示如何集成Redis做数据预热。

---
+++ data_preheating/data_preheating/src/main/java/com/hanson/datapreheat/DataPreheatingApplication.java
@@ -0,0 +1,13 @@
+package com.hanson.datapreheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPreheatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataPreheatingApplication.class, args);
+    }
+
+}
