fab5f9b7b7c158564367d42d11ad2da8d8813390
huang.hanson/黄忠_沪_HQDV <hanson.huang@51job.com>
2024-07-18 18:04:46 +0800

    feat(logger-demo): 配置日志管理，添加LoggerHandler实现日志输出
    - 配置logback.xml以实现日志的控制台和文件输出。
    - 创建LoggerHandler类，实现InitializingBean接口，在初始化时进行日志演示操作。- 相关的logback配置包括日志文件路径、滚动策略和日志级别设置。
    -此外，还包括LoggerDemoApplication和LoggerDemoApplicationTests的创建，以及pom.xml中相关依赖的添加。
    
    该提交添加了日志管理的配置和实现，为后续的日志记录功能提供了基础。

---
+++ logger-demo/logger-demo/src/main/java/com/hansom/logger/LoggerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hansom.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggerDemoApplication.class, args);
+    }
+
+}
